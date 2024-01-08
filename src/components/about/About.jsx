0bc19f153828cb0bec0a9e59647b47d1c4141e39
Kazbekgit <kazbekgit@gmail.com>
2024-01-09 00:51:48 +0300

    About is done, Vans fetch

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,43 @@
+import styles from "./About.module.scss";
+import { Link } from "react-router-dom";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <picture>
+        <source srcSet="/img/about/aboutImg.jpg 1x, /img/about/aboutImg@2x.jpg 2x" />
+        <img
+          src="/img/about/aboutImg.jpg"
+          alt="man on vans watching stars"
+          width="549px"
+          height="233px"
+        />
+      </picture>
+      <div className={styles.wrapper}>
+        <h1 className={styles.mainTitle}>
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p className={styles.tagline}>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch.
+          <br /> (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+
+        <div className={styles.linkWrapper}>
+          <p>Your destination is waiting.</p>
+          <p>Your van is ready.</p>
+          <Link to="/vans" className={styles.link}>
+            Explore our vans
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/components/vans/Vans.jsx
+++ src/components/vans/Vans.jsx
@@ -1,9 +1,32 @@
-import styles from './Vans.module.scss'
+import styles from "./Vans.module.scss";
+import "../../../server";
+import { useEffect, useState } from "react";
 
 const Vans = () => {
-  return (
-    <div className={styles.wrapper}>Vans</div>
-  )
-}
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((resp) => resp.json())
+      .then((data) => setVans(data.vans));
+  }, []);
 
-export default Vans
+  const [vans, setVans] = useState([]);
+  const vansTypes = ["Simple", "Luxury", "Rugged"];
+  const vansElements = vans.map((van, index) => {
+    console.log(van);
+  });
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Explore our van options</h1>
+      <div className={styles.navWrapper}>
+        <ul className={styles.list}>
+          {vansTypes.map((vanType, index) => {
+            return <li key={index}>{vanType}</li>;
+          })}
+        </ul>
+        <button className={styles.clear}>Clear filters</button>
+      </div>
+    </div>
+  );
+};
+
+export default Vans;
