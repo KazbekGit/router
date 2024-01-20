2fb0de57a7306cc59fcf72cf5b038fd59dc43aab
Kazbekgit <kazbekgit@gmail.com>
2024-01-20 23:49:32 +0300

    useSearchParams set params

--- src/components/vans/Vans.jsx
+++ src/components/vans/Vans.jsx
@@ -2,6 +2,7 @@ import styles from "./Vans.module.scss";
 import "../../../server";
 import { useEffect, useState } from "react";
 import Van from "../van/Van";
+import { useSearchParams } from "react-router-dom";
 
 const Vans = () => {
   useEffect(() => {
@@ -10,12 +11,31 @@ const Vans = () => {
       .then((data) => setVans(data.vans));
   }, []);
 
+  const [searchParams, setSearchParams] = useSearchParams({ type: "" });
+
   const [vans, setVans] = useState(null);
   if (!vans) return <p>Loading data...</p>;
-  
+
+  const handleFilterParams = (key, value) => {
+    setSearchParams((prev) => {
+      if (value === null) {
+        prev.delete(key);
+      } else {
+        prev.set(key, value);
+      }
+      return prev
+    });
+  };
+
+  const typeFilter = searchParams.get("type");
+  const filteredVans = typeFilter
+    ? vans.filter((van) => {
+        return van.type.toLowerCase() === typeFilter;
+      })
+    : vans;
+
   const vansTypes = ["simple", "luxury", "rugged"];
-  const vansItems = vans?.map((van) => {
-    console.log(van);
+  const vansItems = filteredVans.map((van) => {
     return <Van key={van.id} van={van} />;
   });
   return (
@@ -24,10 +44,19 @@ const Vans = () => {
       <div className={styles.navWrapper}>
         <ul className={styles.list}>
           {vansTypes.map((vanType, index) => {
-            return <li key={index}>{vanType}</li>;
+            return (
+              <li key={index} onClick={() => handleFilterParams("type", vanType)}>
+                {vanType}
+              </li>
+            );
           })}
         </ul>
-        <button className={styles.clear}>Clear filters</button>
+        <button
+          className={styles.clear}
+          onClick={() => handleFilterParams("type", null)}
+        >
+          Clear filters
+        </button>
       </div>
       <ul className={styles.vansList}>{vansItems}</ul>
     </div>
